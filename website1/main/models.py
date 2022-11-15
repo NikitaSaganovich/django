d9ab5e8713836d076a1941cbe17e6ba8ef2e2107
Nikita Saganovich <nikis2345@mail.ru>
2022-11-15 20:20:44 +0300

    15.11 Cозадл новые поля для модели.

--- website1/main/models.py
+++ website1/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.model):
+    title = models.Charfield('Название', max_length = 50)
+    task = models.textField('Описание')
+    date = models.Charfield('Дата изменения', max_length = 20)
+    name = models.Charfield('Имя редактора', max_length = 30)
+
+    def __str__(self):
+        return self.Title
