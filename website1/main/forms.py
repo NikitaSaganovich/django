a30d99afe63db0612d2582b45b8676326cb378e3
Nikita Saganovich <nikis2345@mail.ru>
2022-12-21 22:09:22 +0300

    21.12(2)

--- website1/main/forms.py
+++ website1/main/forms.py
@@ -1,7 +1,14 @@
+from dataclasses import fields
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput, Textarea
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields=("title", "task")
+        fields = {"title", "task", "date", "name"}
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
+                   "task": Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите описание'}),
+                   "date": TextInput(attrs={'class': 'form-control', 'placeholder': 'Дата'}),
+                   "name": TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваше имя'})
+                   }
+
